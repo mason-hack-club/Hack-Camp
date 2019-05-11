c78683521f311b13904e44254b76442f1eceea6a
Dalton Craven <20dalton00@gmail.com>
2019-05-10 23:15:24 -0400

    fix form pt. 3

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,14 @@
-/* attach a submit handler to the form */
-$("#register").submit(function(event) {
-  /* stop form from submitting normally */
-  event.preventDefault();
+$("#register").submit(function(event){
+	event.preventDefault(); //prevent default action
+	var post_url = $(this).attr("action"); //get form action url
+	var request_method = $(this).attr("method"); //get form GET/POST method
+	var form_data = $(this).serialize(); //Encode form elements for submission
 
-  /* get the action attribute from the <form action=""> element */
-  var $form = $(this);
-
-  var posting = $.post("https://dash.zane.sh/api/v1/events/mason-hack-camp-2019/attendees", $form.serialize());
-
-  /* Alerts the results */
-  posting.done(function(data) {
+	$.ajax({
+		url : post_url,
+		type: request_method,
+		data : form_data
+	}).done(function(response){
     window.location.replace("/register/confirm");
-  });
+	});
 });
